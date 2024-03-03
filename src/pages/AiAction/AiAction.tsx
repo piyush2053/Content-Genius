1dfda30537735a42a8aae3daa1d326bff7972be2
piyush.patel <piyush.patel@rws.com>
2024-03-03 15:46:55 +0530

    Warning for single word

--- src/pages/AiAction/AiAction.tsx
+++ src/pages/AiAction/AiAction.tsx
@@ -5,12 +5,14 @@ import '../../index.css';
 import Lottie from 'react-lottie'
 import animationData from './loader/loder.json'
 import animationDataPopper from './loader/poppers.json'
+import animationDataWarning from './loader/warning.json'
 
 const AiAction = () => {
   const inputRef = useRef(null);
   const [definitions, setDefinitions] = useState<{ word: string, definition: string, example: string }[]>([]);
   const [loading, setloading] = useState(false);
   const [poppers, setPoppers] = useState(false);
+  const [warning, setWarning] = useState(false);
 
   const handleButtonClick = () => {
     setloading(true)
@@ -42,16 +44,33 @@ const AiAction = () => {
         console.error('Error:', error);
       });
   }
-
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
+      setWarning(false)
       handleButtonClick();
+    } else {
+      const trimmedValue = inputRef.current.value.trim();
+      if (trimmedValue.includes(' ') && event.key !== 'Backspace' && event.key !== 'Shift' && event.key !== 'ArrowLeft' && event.key !== 'Control' && !(event.ctrlKey && event.key === 'a')) {
+        setWarning(true)
+        event.preventDefault();
+      } else {
+        setWarning(false)
+      }
     }
   }
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+  const defaultOptionsWarning = {
+    loop: true,
+    autoplay: true,
+    animationData: animationDataWarning,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
     }
@@ -78,8 +97,9 @@ const AiAction = () => {
           Content Dictionary
         </h1>
       </div>
-      <div className='mx-5 p-1 mt-10 rounded-xl bg-[#141414] flex h-[100px]'>
+      <div className={`mx-5 p-1 mt-10 rounded-xl bg-[#141414] ${warning ? 'h-[120px]' : 'h-[100px]'}`}>
         <input ref={inputRef} type='text' className='flex-grow bg-transparent rounded border border-none p-7' placeholder='Give me a Word Please ' onKeyDown={handleKeyPress}></input>
+        {warning ? <div className='fade in flex ml-3'><Typography className='fade in inverseColor text-bold' >Single Word Please</Typography></div> : null}
       </div>
       <div className="max-w-7xl mx-auto flex flex-col items-center text-center mt-5">
         {loading
@@ -90,12 +110,17 @@ const AiAction = () => {
             width={50}
           /></div>
           :
-          <button className='bg-[#fff] flex text-[#8c8c8c] text-[20px] font-semibold px-4 py-1 rounded-full border border-white transition-all duration-300 hover:bg-transparent hover:text-[#fff] hover:animate-pulse' onClick={handleButtonClick}>
+          <>{warning ? <div className='fade in flex justify-end'><Lottie
+            options={defaultOptionsWarning}
+            height={60}
+            width={60}
+          /></div> : <button className='fade in bg-[#fff] flex text-[#8c8c8c] text-[20px] font-semibold px-4 py-1 rounded-full border border-white transition-all duration-300 hover:bg-transparent hover:text-[#fff] hover:animate-pulse' onClick={handleButtonClick}>
             Magic <FaMagic className='ml-2 mt-1' />
-          </button>}
+          </button>}</>
+        }
       </div>
       <div className="fade-in max-w-7xl mx-auto mt-5 px-7" style={{ position: "relative" }}>
-      <div className="poppers" style={{ position: 'absolute', top: 0, left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
+        <div className="poppers" style={{ position: 'absolute', top: 0, left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
           {poppers ? <Lottie
             options={popperOptions}
             height={500}
